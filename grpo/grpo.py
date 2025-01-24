33883c8263fa7a7bf7ecda8acc990315a218b53a
jackfsuia <53132029+jackfsuia@users.noreply.github.com>
2025-01-25 01:47:41 +0800

    Update grpo.py

--- grpo/grpo.py
+++ grpo/grpo.py
@@ -98,7 +98,7 @@ class GRPOConfig(PPOConfig):
     
     lora_bias:str = field(default="none")
 
-    train_dataset_name :str = field(default="/data/hh-rlhf")
+    train_dataset_name :str = field(default="Anthropic/hh-rlhf")
     train_dataset_split:str = field(default="train[:100%]")
 
     advantage_whiten :bool = field(default= False)# this is default to be False since GRPO has provided a baseline
