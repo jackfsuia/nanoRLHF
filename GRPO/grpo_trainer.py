0f78c046ee988d2fdab2052e02c9ea68856a1322
jackfsuia <53132029+jackfsuia@users.noreply.github.com>
2025-02-20 06:17:33 +0800

    Update grpo_trainer.py

--- GRPO/grpo_trainer.py
+++ GRPO/grpo_trainer.py
@@ -517,7 +517,7 @@ class GRPOTrainer(Trainer):
                 log_scores =  log_scores.view(-1,args.grpo_sample_N)
                 log_scores = log_scores[torch.arange(batch_size), random_indices]
                 log_scores = log_scores.view(-1)
-                responses_decoded = [responses_decoded[i * 4 + idx] for i, idx in enumerate(random_indices)]
+                responses_decoded = [responses_decoded[i * args.grpo_sample_N + idx] for i, idx in enumerate(random_indices)]
 
 
                 gc.collect()
